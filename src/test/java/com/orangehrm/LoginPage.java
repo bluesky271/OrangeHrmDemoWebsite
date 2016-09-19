3fb35de7ff92d6b89520e90751801513d9d0e061
Ngou Long Kam <ngoulongkam@Ngous-Mac-mini.local>
2016-09-19 23:46:25 +0100

    added Login page

--- src/test/java/com/orangehrm/LoginPage.java
+++ src/test/java/com/orangehrm/LoginPage.java
@@ -1,11 +1,46 @@
 package com.orangehrm;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
 public class LoginPage {
 
-    public static void main (String[] args) {
+    WebDriver driver;
+    String baseUrl = "https://enterprise-demo.orangehrmlive.com/symfony/web/index.php/auth/login";
+
+    String adminUsername = "sysadmin";
+    String adminPassword = "sysadmin";
+    WebElement username;
+    WebElement password;
+    WebElement loginButton;
 
 
-
+    @BeforeTest
+    public void navigateToLoginPage() {
+        driver = new FirefoxDriver();
+        driver.get(baseUrl);
     }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test
+    public void enterAdminUsernamePassword() {
+        username = driver.findElement(By.xpath(".//*[@id='txtUsername']"));
+        username.sendKeys(adminUsername);
+        password = driver.findElement(By.xpath(".//*[@id='txtPassword']"));
+        password.sendKeys(adminPassword);
+        loginButton = driver.findElement(By.xpath(".//*[@id='btnLogin']"));
+        loginButton.click();
+    }
+
+
 
 }
