0fea3e3a42285d949d2bcccc9677018b03c88d06
Ngou Long Kam <ngoulongkam@Ngous-Mac-mini.local>
2016-09-21 22:35:01 +0100

    minor changes

--- src/test/java/com/orangehrm/Basepage.java
+++ src/test/java/com/orangehrm/Basepage.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 import org.testng.Assert;
 import util.Support;
 
@@ -15,13 +13,13 @@ public class Basepage extends Support {
     public WebDriver driver = new FirefoxDriver();
     public String baseUrl = "https://enterprise-demo.orangehrmlive.com/symfony/web/index.php/auth/login";
 
-    @BeforeTest
+    @BeforeSuite
     public void goToLoginPage() {
 
         driver.get(baseUrl);
     }
 
-    @AfterTest
+    @AfterSuite
     public void quitDriver() {
 
         driver.quit();
